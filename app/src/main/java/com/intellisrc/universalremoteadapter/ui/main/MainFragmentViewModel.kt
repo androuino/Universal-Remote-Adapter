b9b3db20b435e6bd1c9109bea5e8feab171178f1
sem <sem@aju-cil.net>
2020-06-23 15:51:00 +0900

    working on BLE connection: This push has some error and will definitely crash the app

--- app/src/main/java/com/intellisrc/universalremoteadapter/ui/main/MainFragmentViewModel.kt
+++ app/src/main/java/com/intellisrc/universalremoteadapter/ui/main/MainFragmentViewModel.kt
@@ -2,6 +2,10 @@ package com.intellisrc.universalremoteadapter.ui.main
 
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.*
 import com.github.ivbaranov.rxbluetooth.RxBluetooth
 import com.intellisrc.universalremoteadapter.Constants
@@ -9,14 +13,17 @@ import com.intellisrc.universalremoteadapter.ui.MainActivity
 import com.intellisrc.universalremoteadapter.ui.base.BaseViewModel
 import com.intellisrc.universalremoteadapter.ui.remote_controller.RemoteControllerFragmentKey
 import com.polidea.rxandroidble2.RxBleClient
+import com.polidea.rxandroidble2.scan.ScanFilter
 import com.zhuinden.simplestack.Backstack
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class MainFragmentViewModel @Inject constructor(private val backstack: Backstack, val rxBluetooth: RxBluetooth, val rxBleClient: RxBleClient, private val mainActivity: MainActivity) : BaseViewModel(), Observer<String> {
     private val compositeDisposable: CompositeDisposable = CompositeDisposable()
     private val bluetoothDevicesList = MutableLiveData<ArrayList<BluetoothDevice>>()
@@ -42,6 +49,15 @@ class MainFragmentViewModel @Inject constructor(private val backstack: Backstack
                         }
                 )
                 rxBluetooth.startDiscovery()
+
+                if (rxBleClient.isScanRuntimePermissionGranted) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        scanBleDevices()
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .doFinally { dispose() }
+                            .subscribe({}, {})
+                    }
+                }
             }
         }
     }
@@ -75,6 +91,27 @@ class MainFragmentViewModel @Inject constructor(private val backstack: Backstack
      */
     fun connectToBleDevice() {
 
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun scanBleDevices(): Observable<ScanResult> {
+        val scanSettings = ScanSettings.Builder()
+            .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+            .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES)
+            .build()
+
+        val scanFilter = ScanFilter.Builder()
+            // .setDeviceAddress("B4:99:4C:34:DC:8B")
+            // add custom filters if needed
+            .build()
+
+        return rxBleClient.scanBleDevices(scanSettings, scanFilter)
+    }
+
+    private fun dispose() {
+        //scanDisposable = null
+        //resultsAdapter.clearScanResults()
+        //updateButtonUIState()
     }
 
     fun onDestroy() {
